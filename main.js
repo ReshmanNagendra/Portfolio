31a40f5542476b9a1621e923417030e413ec6bdd
ReshmanNagendra <reshman.nagendra.s.140@kalvium.community>
2026-02-19 14:30:50 +0530

    added 2nd project

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ let project = document.getElementById("1");
 project.addEventListener("click", () => {
     window.location.href = "https://reshmannagendra.github.io/Typing_Speed_Game/";
 });
+// added 2nd project to be clickable
+let project2 = document.getElementById("2");
+
+project2.addEventListener("click", () => {
+    window.location.href = "https://reshmannagendra.github.io/Galactic-Guardian-3D/";
+});
